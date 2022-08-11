4c03f3bd4ba1139aee6ac7422d17dc6fc0bbac3b
deimosrasiv <deimosrasiv@gmail.com>
2022-08-11 14:23:24 -0400

    add modal to contact

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,20 @@ function enviar() {
         alert("Debe Ingresar Nombre y Correo, para que podamos contactarlo")
     }
 }
+
+
+
+function openmodal() {
+
+    var email = document.getElementById("email").value;
+    var nombre = document.getElementById("nombre").value;
+    //console.log(nombre);
+
+    $('#ModalCorreo').modal('show'); // abrir
+
+    document.getElementById("modalnombre").innerHTML = nombre;
+    document.getElementById("email_user").innerHTML = email;
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("email").value = "";
+}
